1efd09c526fc9034a93fe644c1d72f9b59790278
andyalphard10122005 <Andoalpot@gmail.com>
2022-11-04 15:22:37 +0700

    Andy Alpharf

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use App\Http\Controllers\Adri;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers;
-use App\Http\Controllers\Khairan_Nail;p
+use App\Http\Controllers\Khairan_Nail;
+use App\Http\Controllers\Andy;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -24,4 +25,5 @@ Route::get('/', function () {
 Route::get('members/nail', [App\Http\Controllers\Khairan_Nail::class, 'index'])->name('Khairan_nail.nail');
 Route::get('members/sheyla', [App\Http\Controllers\Sheyla::class, 'index'])->name('Sheyla.sheyla');
 Route::get('/members/bagas', [Adri::class, 'index']);
+Route::get('/members/Andy', [Andy::class, 'index']);
 
